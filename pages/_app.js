45a6ec711164558bc151d8e69c5103e83660faff
rawbethwebsites <rawbethwebsites@gmail.com>
2026-01-19 14:16:30 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,6 @@ import '../components/common/GhostCursor.css'
 import '../components/common/GridScan.css'
 import '../components/common/GridDistortion.css'
 import '../components/common/FlowingMenu.css'
-import '../components/common/DomeGallery.css'
 import TargetCursor from '../components/common/TargetCursor'
 
 export default function App({ Component, pageProps }) {
