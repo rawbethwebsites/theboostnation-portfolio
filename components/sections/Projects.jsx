b26b462c936c1e0930274a0c2d422819e109a15e
rawbethwebsites <rawbethwebsites@gmail.com>
2026-01-19 15:04:51 +0100

    yes

--- components/sections/Projects.jsx
+++ components/sections/Projects.jsx
@@ -3,7 +3,7 @@ import DomeGallery from '../DomeGallery'
 
 export default function Projects(){
   return (
-    <section id="projects" style={{ width: '100vw', height: '100vh', position: 'relative', backgroundColor: '#000' }}>
+    <section id="projects" style={{ width: '100vw', height: '100vh', position: 'relative' }}>
       <div style={{
         position: 'absolute',
         top: '50px',
@@ -18,36 +18,14 @@ export default function Projects(){
       }}>
         Our Projects
       </div>
-      <div style={{
-        position: 'absolute',
-        top: '150px',
-        left: '50%',
-        transform: 'translateX(-50%)',
-        display: 'grid',
-        gridTemplateColumns: 'repeat(4, 200px)',
-        gap: '20px',
-        zIndex: 5
-      }}>
-        {[1,2,3,4,5,6,7,8].map(num => (
-          <div key={num} style={{
-            width: '200px',
-            height: '150px',
-            borderRadius: '10px',
-            overflow: 'hidden',
-            boxShadow: '0 5px 15px rgba(0,0,0,0.3)'
-          }}>
-            <img
-              src={`/images/projects/project-${num}.jpg`}
-              alt={`Project ${num}`}
-              style={{
-                width: '100%',
-                height: '100%',
-                objectFit: 'cover'
-              }}
-            />
-          </div>
-        ))}
-      </div>
+      <DomeGallery
+        fit={0.5}
+        minRadius={300}
+        maxVerticalRotationDeg={0}
+        segments={34}
+        dragDampening={2}
+        grayscale={false}
+      />
     </section>
   )
 }
